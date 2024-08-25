e82ae52d8133481e0a2c8b5446bd25405d7242e6
Gustavo Linares Villegas <guslinaresv@gmail.com>
2024-08-25 13:23:57 -0600

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Login } from "./Views/Login/Login"
+
+const App = () => {
+  return (
+    <>
+    < Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/Views/Login/Login.jsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
+
+export const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-full"
+    style={{
+      background: "radial-gradient(circle at center, #0569cd, #002d5a, #001430)",
+    }}>
+      <div className="flex w-96 h-52 m-12">
+        <img src="/public/wallpaperflare.com_wallpaper.jpg" alt="Spartan" style={{aspectRatio: 1/1, contain: "content"}}/>
+      </div>
+      <Card className="w-[550px] z-10 bg-transparent backdrop-blur-lg border-none">
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email" className="text-white">Email / Número Tel.</Label>
+                <Input 
+                  id="email" 
+                  type="email" 
+                  placeholder="nombre@ejemplo.com" 
+                  required 
+                  className="bg-white text-black border-none focus:border-none focus:outline-none"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password" className="text-white">Contraseña</Label>
+                <Input 
+                  id="password" 
+                  type="password" 
+                  required 
+                  className="bg-white text-black border-none focus:border-none focus:outline-none"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full bg-customBlue text-white hover:bg-customBlueStrong">Entrar</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
